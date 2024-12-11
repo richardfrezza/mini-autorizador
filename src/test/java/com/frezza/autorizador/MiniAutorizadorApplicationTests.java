1ae4d9e2adfd36d1485c4b00c768ab6f872e0875
richard.frezza <richardfrezza@gmail.com>
2024-12-11 13:29:26 -0300

    initial commit

---
+++ src/test/java/com/frezza/autorizador/MiniAutorizadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frezza.autorizador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniAutorizadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
