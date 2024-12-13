e20bb2809fa19c63c72f489e297c919584fa59ba
richard.frezza <richardfrezza@gmail.com>
2024-12-13 00:45:16 -0300

    feat: alteracao coluna password

--- src/main/resources/db/migration/V1__CARDS.sql
+++ src/main/resources/db/migration/V1__CARDS.sql
@@ -1,7 +1,7 @@
 create table if not exists cards (
    id int primary key auto_increment,
    number varchar(16) not null,
-   password varchar(50) not null,
+   password varchar(60) not null,
    balance decimal(12,2) not null default 0,
    dh_created_at timestamp null,
    dh_updated_at timestamp null,
