a77a585c7d6dc87f016b594c7e1994aba54c1b1c
richard.frezza <richardfrezza@gmail.com>
2024-12-13 00:51:30 -0300

    feat: inclusao repository

---
+++ src/main/java/com/frezza/autorizador/persistence/repository/CardRepository.java
@@ -0,0 +1,20 @@
+package com.frezza.autorizador.persistence.repository;
+
+import com.frezza.autorizador.persistence.model.Card;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+public interface CardRepository extends JpaRepository<Card, Integer> {
+
+   Card findByNumber(String number);
+
+   @Query("SELECT c.balance FROM Card c WHERE c.number = :number")
+   Optional<BigDecimal> findBalanceByNumber(@Param("number") String number);
+
+   boolean existsByNumber(String number);
+
+}
