0434fefbf8a443fa495e8d507148ad92b5b93333
richard.frezza <richardfrezza@gmail.com>
2024-12-16 01:26:26 -0300

    feat: configurando testes unitarios/integracao

---
+++ src/test/java/com/frezza/autorizador/utils/AutorizadorIntegrationTestConfig.java
@@ -0,0 +1,23 @@
+package com.frezza.autorizador.utils;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@AutoConfigureMockMvc()
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@ActiveProfiles("test")
+public @interface AutorizadorIntegrationTestConfig {
+}
---
+++ src/test/java/com/frezza/autorizador/utils/Models.java
@@ -0,0 +1,35 @@
+package com.frezza.autorizador.utils;
+
+import com.frezza.autorizador.persistence.dto.CardDto;
+import com.frezza.autorizador.persistence.dto.TransactionDto;
+import com.frezza.autorizador.persistence.model.Card;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.math.BigDecimal;
+
+public class Models {
+
+    public static CardDto createCardDto() {
+        CardDto dto = new CardDto();
+        dto.setNumeroCartao("9999999999999999");
+        dto.setSenha("admin");
+        return dto;
+    }
+
+    public static Card createCard() {
+        Card card = new Card();
+        card.setNumber("9999999999999999");
+        card.setPassword("admin");
+        card.setBalance(BigDecimal.valueOf(500.00));
+        return card;
+    }
+
+    public static TransactionDto createTransactionDto() {
+        TransactionDto dto = new TransactionDto();
+        dto.setNumeroCartao("9999999999999999");
+        dto.setValor(BigDecimal.valueOf(10));
+        dto.setSenhaCartao("admin");
+        return dto;
+    }
+
+}
